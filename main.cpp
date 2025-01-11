15915d9cd41a116dbb5fa93c6031fdd6069142ed
Hassan Raza <razah1532@gmail.com>
2025-01-11 16:48:47 +0500

    updated

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main() {
         cin >> choice;
         switch (choice) {
             case 1:
-               // 
+               TicTacToe();
                 break;
             case 2:
                //  snakeGame();
@@ -41,4 +41,92 @@ int main() {
         }
     } while (choice != 0);
     return 0;
+}
+
+
+
+void displayBoard(char board[3][3]);
+bool checkWin(char board[3][3], char player);
+bool isFull(char board[3][3]);
+
+void TicTacToe() {
+    char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+    char currentPlayer = 'X';
+    int move;
+    bool gameWon = false;
+
+    cout << "Welcome to Tic Tac Toe!" << endl;
+    displayBoard(board);
+
+    while (!gameWon && !isFull(board)) {
+        cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+        cin >> move;
+
+        if (move < 1 || move > 9) {
+            cout << "Invalid move. Please try again." << endl;
+            continue;
+        }
+
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+
+        if (board[row][col] == 'X' || board[row][col] == 'O') {
+            cout << "Spot already taken. Please try again." << endl;
+            continue;
+        }
+
+        board[row][col] = currentPlayer;
+        displayBoard(board);
+
+        if (checkWin(board, currentPlayer)) {
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            gameWon = true;
+        } else {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+
+    if (!gameWon) {
+        cout << "It's a draw!" << endl;
+    }
+}
+
+void displayBoard(char board[3][3]) {
+    cout << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << endl;
+        if (i < 2) cout << "--+---+--" << endl;
+    }
+    cout << endl;
+}
+
+bool checkWin(char board[3][3], char player) {
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) || 
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+
+    return false;
+}
+
+bool isFull(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
 }
